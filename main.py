c400a81be234343c29c884f72944c7706ad6cd4f
haanhtuanetsy <haanhtuanetsy@gmail.com>
2026-04-29 20:52:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import re
 
 EMAIL = "haanhtuanetsy@gmail.com"
-PASSWORD = "unaciiagaapxsouxD"
+PASSWORD = "slzzfsvttjqpjykt"
 
 BOT_TOKEN = "8687189308:AAG0IKJPF84WnsXB6DxGKvcltu81222njzY"
 CHAT_ID = "7242802148"
